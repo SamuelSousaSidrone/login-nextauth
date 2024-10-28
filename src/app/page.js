a5b100eee3c3a717230e4605597aeafa84abb71b
Samuel de Sousa Sidrone <samuelsousasidrone@gmail.com>
2024-10-28 16:30:49 -0300

    lol

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use client";
+import LoginForm from "@/app/components/LoginForm";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center m-4">
+      <h1 className="text-3x1 my-3">Hey, Time to Sign In</h1>
+      <LoginForm />
+    </div>
+  );
+}
